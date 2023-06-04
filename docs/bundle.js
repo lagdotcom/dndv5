29e3bffe5e76c9d2e32275a9e5b880a9b084637c
Paul Davies <lagdotcom@gmail.com>
2023-06-04 14:27:26 +0100

    added Devotion stubs

--- docs/bundle.js
+++ docs/bundle.js
@@ -4193,6 +4193,40 @@ You can use this feature a number of times equal to your Charisma modifier (a mi
   var Sanctuary_default = Sanctuary;
 
   // src/classes/paladin/Devotion/index.ts
+  var SacredWeapon = notImplementedFeature(
+    "Channel Divinity: Sacred Weapon",
+    `As an action, you can imbue one weapon that you are holding with positive energy, using your Channel Divinity. For 1 minute, you add your Charisma modifier to attack rolls made with that weapon (with a minimum bonus of +1). The weapon also emits bright light in a 20-foot radius and dim light 20 feet beyond that. If the weapon is not already magical, it becomes magical for the duration.
+
+You can end this effect on your turn as part of any other action. If you are no longer holding or carrying this weapon, or if you fall unconscious, this effect ends.`
+  );
+  var TurnTheUnholy = notImplementedFeature(
+    "Channel Divinity: Turn the Unholy",
+    `As an action, you present your holy symbol and speak a prayer censuring fiends and undead, using your Channel Divinity. Each fiend or undead that can see or hear you within 30 feet of you must make a Wisdom saving throw. If the creature fails its saving throw, it is turned for 1 minute or until it takes damage.
+
+A turned creature must spend its turns trying to move as far away from you as it can, and it can't willingly move to a space within 30 feet of you. It also can't take reactions. For its action, it can use only the Dash action or try to escape from an effect that prevents it from moving. If there's nowhere to move, the creature can use the Dodge action.`
+  );
+  var AuraOfDevotion = notImplementedFeature(
+    "Aura of Devotion",
+    `Starting at 7th level, you and friendly creatures within 10 feet of you can't be charmed while you are conscious.
+
+At 18th level, the range of this aura increases to 30 feet.`
+  );
+  var PurityOfSpirit = notImplementedFeature(
+    "Purity of Spirit",
+    `Beginning at 15th level, you are always under the effects of a protection from evil and good spell.`
+  );
+  var HolyNimbus = notImplementedFeature(
+    "Holy Nimbus",
+    `At 20th level, as an action, you can emanate an aura of sunlight. For 1 minute, bright light shines from you in a 30-foot radius, and dim light shines 30 feet beyond that.
+
+Whenever an enemy creature starts its turn in the bright light, the creature takes 10 radiant damage.
+
+In addition, for the duration, you have advantage on saving throws against spells cast by fiends or undead.
+
+Once you use this feature, you can't use it again until you finish a long rest.
+
+`
+  );
   var OathSpells = bonusSpellsFeature(
     "Oath Spells",
     `You gain oath spells at the paladin levels listed.`,
@@ -4216,7 +4250,12 @@ You can use this feature a number of times equal to your Charisma modifier (a mi
   var Devotion = {
     className: "Paladin",
     name: "Oath ofDevotion",
-    features: /* @__PURE__ */ new Map([[3, [OathSpells]]])
+    features: /* @__PURE__ */ new Map([
+      [3, [OathSpells, SacredWeapon, TurnTheUnholy]],
+      [7, [AuraOfDevotion]],
+      [15, [PurityOfSpirit]],
+      [20, [HolyNimbus]]
+    ])
   };
   var Devotion_default = Devotion;
 
--- src/classes/paladin/Devotion/index.ts
+++ src/classes/paladin/Devotion/index.ts
@@ -1,10 +1,55 @@
-import { bonusSpellsFeature } from "../../../features/common";
+import {
+  bonusSpellsFeature,
+  notImplementedFeature,
+} from "../../../features/common";
 import ProtectionFromEvilAndGood from "../../../spells/level1/ProtectionFromEvilAndGood";
 import Sanctuary from "../../../spells/level1/Sanctuary";
 import PCSubclass from "../../../types/PCSubclass";
 import { PaladinSpellcasting } from "../common";
 
-// TODO Sacred Weapon, Turn the Unholy, Aura of Devotion, Purity of Spirit, Holy Nimbus
+// TODO
+const SacredWeapon = notImplementedFeature(
+  "Channel Divinity: Sacred Weapon",
+  `As an action, you can imbue one weapon that you are holding with positive energy, using your Channel Divinity. For 1 minute, you add your Charisma modifier to attack rolls made with that weapon (with a minimum bonus of +1). The weapon also emits bright light in a 20-foot radius and dim light 20 feet beyond that. If the weapon is not already magical, it becomes magical for the duration.
+
+You can end this effect on your turn as part of any other action. If you are no longer holding or carrying this weapon, or if you fall unconscious, this effect ends.`
+);
+
+// TODO
+const TurnTheUnholy = notImplementedFeature(
+  "Channel Divinity: Turn the Unholy",
+  `As an action, you present your holy symbol and speak a prayer censuring fiends and undead, using your Channel Divinity. Each fiend or undead that can see or hear you within 30 feet of you must make a Wisdom saving throw. If the creature fails its saving throw, it is turned for 1 minute or until it takes damage.
+
+A turned creature must spend its turns trying to move as far away from you as it can, and it can't willingly move to a space within 30 feet of you. It also can't take reactions. For its action, it can use only the Dash action or try to escape from an effect that prevents it from moving. If there's nowhere to move, the creature can use the Dodge action.`
+);
+
+// TODO
+const AuraOfDevotion = notImplementedFeature(
+  "Aura of Devotion",
+  `Starting at 7th level, you and friendly creatures within 10 feet of you can't be charmed while you are conscious.
+
+At 18th level, the range of this aura increases to 30 feet.`
+);
+
+// TODO
+const PurityOfSpirit = notImplementedFeature(
+  "Purity of Spirit",
+  `Beginning at 15th level, you are always under the effects of a protection from evil and good spell.`
+);
+
+// TODO
+const HolyNimbus = notImplementedFeature(
+  "Holy Nimbus",
+  `At 20th level, as an action, you can emanate an aura of sunlight. For 1 minute, bright light shines from you in a 30-foot radius, and dim light shines 30 feet beyond that.
+
+Whenever an enemy creature starts its turn in the bright light, the creature takes 10 radiant damage.
+
+In addition, for the duration, you have advantage on saving throws against spells cast by fiends or undead.
+
+Once you use this feature, you can't use it again until you finish a long rest.
+
+`
+);
 
 const OathSpells = bonusSpellsFeature(
   "Oath Spells",
@@ -30,6 +75,11 @@ const OathSpells = bonusSpellsFeature(
 const Devotion: PCSubclass = {
   className: "Paladin",
   name: "Oath ofDevotion",
-  features: new Map([[3, [OathSpells]]]),
+  features: new Map([
+    [3, [OathSpells, SacredWeapon, TurnTheUnholy]],
+    [7, [AuraOfDevotion]],
+    [15, [PurityOfSpirit]],
+    [20, [HolyNimbus]],
+  ]),
 };
 export default Devotion;
