0f7ebc4b5e506ee32b7ce94e1f68b178cda2691f
Paul Davies <lagdotcom@gmail.com>
2023-11-09 14:35:15 +0000

    lol

--- analyse.mjs
+++ analyse.mjs
@@ -5,9 +5,10 @@ import fs from "fs";
 
 import getBuildConfig from "./buildConfig.mjs";
 
-const settings = getBuildConfig({ minify: true, metafile: true });
+const settings = getBuildConfig("build", { minify: true, metafile: true });
 const result = await build(settings);
 
 // console.log(await analyzeMetafile(result.metafile, { verbose: false }));
 fs.writeFileSync("build-meta.json", JSON.stringify(result.metafile));
 console.log("Wrote: build-meta.json");
+console.log("Use it here: https://esbuild.github.io/analyze/");
