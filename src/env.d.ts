a4f1a338559355720cd248e0f0d004c1e620fa87
Paul Davies <lagdotcom@gmail.com>
2024-01-07 13:39:16 +0000

    oops

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,6 @@
 declare const MODE: "build" | "watch" | "test";
 
 interface Window {
-  g: import("./Engine").Engine;
+  g: import("./Engine").default;
   state: import("./ui/utils/state").UIState;
 }
